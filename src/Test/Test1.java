0666ac1bee34f1b592d86987352e437003b5c63c
Wenhao LUO <wenhao.luo4@etu.univ-lorraine.fr>
2022-10-28 14:57:30 -0400

    add test

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -6,6 +6,11 @@ public class Test1 {
     public static void main(String[] args) {
         Board b = new Board();
         b.newGame();
+
+        System.out.println("print the board (init):");
         System.out.println(b);
+
+        System.out.println("print the available moves");
+        System.out.println(b.getAvaiableMoves());
     }
 }
